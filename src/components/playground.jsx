5b4bb70350f5c35f1850fd7cce8027b5d54bdec3
Ke Wang <453587854@qq.com>
2017-12-10 23:36:40 +0800

    绘制拼图碎片

--- src/components/playground.jsx
+++ src/components/playground.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 class Playground extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      piecePositions: null,
+    };
+  }
+
+  componentWillMount() {
+    let shapes = this.props.puzzle.shapes.sort(_ => Math.random() - 0.5);
+    if (!this.state.piecePositions) {
+      let xPointer = 0;
+      this.setState({
+        piecePositions: shapes.map((_, index) => {
+          let pos = ({ x: xPointer, y: 0 });
+          xPointer += +_.props.width;
+          console.log(xPointer);
+          return pos;
+        }),
+      });
+    }
+  }
+
   render() {
     let puzzle = this.props.puzzle;
-    return puzzle.shapes;
+    return <div className="playground">
+      {puzzle.shapes.map((_, index) => {
+        return <div className="puzzle-piece"
+                    style={{
+                      left: this.state.piecePositions[index].x,
+                      top: this.state.piecePositions[index].y,
+                    }}
+                    key={index}
+                    onMouseDown={() => {
+                      console.log('1')
+                    }}>{_}</div>
+      })}
+    </div>
   }
 }
 export default Playground;
--- src/puzzle/fish.jsx
+++ src/puzzle/fish.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Polygon from "../components/polygon.jsx";
 
 let shapes = [
-  <Polygon points={[[0, 50], [50, 0], [100, 50]]} color="rgb(228,0,123)"/>,
+  <Polygon points={[[0, 50], [50, 0], [100, 50]]} color="rgb(228,0,123)" height="50"/>,
   <Polygon points={[[0, 0], [75, 75], [75, 135], [0, 60]]} color="blue" height="135" width="75"/>,
   <Polygon points={[[0, 0], [60, 0], [60, 60], [0, 60]]} color="red" height="60" width="60"/>,
   <Polygon points={[[0, 60], [60, 0], [60, 60]]} color="red" height="60" width="60"/>,
   <Polygon points={[[0, 0], [120, 120], [0, 120]]} color="orange" height="120" width="120"/>,
   <Polygon points={[[0, 0], [120, 0], [120, 120]]} color="rgb(228,0,123)" height="120" width="120"/>,
-  <Polygon points={[[0, 0], [0, 120], [60, 60]]} color="yellow" height="120" width="120"/>,
+  <Polygon points={[[0, 0], [0, 120], [60, 60]]} color="yellow" height="120" width="60"/>,
 ];
 
 export default { shapes };
