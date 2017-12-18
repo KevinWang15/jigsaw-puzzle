a7063edf5c4266f63f10a196cc5afcedc8e5c91f
Ke Wang <453587854@qq.com>
2017-12-18 21:37:19 +0800

    fix drag problem on firefox

--- src/index.js
+++ src/index.js
@@ -8,3 +8,7 @@ let root=document.createElement('div');
 root.className='root';
 document.body.appendChild(root);
 ReactDOM.render(React.createElement(Playground, { puzzle: fishPuzzle }), root);
+
+window.document.ondragstart = () => {
+  return false;
+};
